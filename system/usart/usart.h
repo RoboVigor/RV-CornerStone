ccd09ad056bccede1319e5c0f3a6f0e9c8b5e225
sleepymemory <41004268+sleepymemory@users.noreply.github.com>
2018-08-06 18:20:00 +0800

    0.06

---
+++ system/usart/usart.h
@@ -0,0 +1,16 @@
+#ifndef __USART_H
+#define __USART_H
+#include "stdio.h"	
+#include "stm32f4xx_conf.h"
+
+
+#define USART_REC_LEN  			200        	//定义最大接收字节数 200
+#define EN_USART6_RX 			  1		        //使能（1）/禁止（0）串口3接收
+	  	
+extern u8  USART_RX_BUF[USART_REC_LEN]; //接收缓冲,最大USART_REC_LEN个字节.末字节为换行符 
+extern u16 USART_RX_STA;         		    //接收状态标记	
+
+void uart_init(u32 bound);
+#endif
+
+
