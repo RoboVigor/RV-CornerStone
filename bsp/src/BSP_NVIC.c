c3397c8530cac7b5809b782bd188003a8ef19ecd
tccoin <1774241372@qq.com>
2018-08-12 01:09:23 +0800

    0.1.10 修改了中断优先级配置

--- bsp/src/BSP_NVIC.c
+++ bsp/src/BSP_NVIC.c
@@ -9,34 +9,34 @@ void BSP_NVIC_InitConfig(void)
 {
 	NVIC_InitTypeDef	NVIC_InitStructure;
 	
-	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 	
 	//UART(DBUS)
 	NVIC_InitStructure.NVIC_IRQChannel						        =	USART1_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd					        =	ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority	=	0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority			    =	1;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority	=	8;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority			    =	0;
 	NVIC_Init(&NVIC_InitStructure);
 	USART_ITConfig(USART1, USART_IT_IDLE, ENABLE);
 	
 	//CAN1
 	NVIC_InitStructure.NVIC_IRQChannel                    = CAN1_RX0_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd                 = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority  = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority         = 2;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority  = 8;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority         = 0;
 	NVIC_Init(&NVIC_InitStructure);
 	CAN_ITConfig(CAN1, CAN_IT_FMP0, ENABLE);
 	
 	NVIC_InitStructure.NVIC_IRQChannel                    = CAN2_RX0_IRQn;
 	NVIC_InitStructure.NVIC_IRQChannelCmd                 = ENABLE;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority  = 0;
-	NVIC_InitStructure.NVIC_IRQChannelSubPriority         = 3;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority  = 8;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority         = 0;
 	NVIC_Init(&NVIC_InitStructure);
 	CAN_ITConfig(CAN2, CAN_IT_FMP0, ENABLE);
 	
 	//TIM6
 	NVIC_InitStructure.NVIC_IRQChannel = TIM6_DAC_IRQn;
-  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 1;
+  NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 9;
   NVIC_InitStructure.NVIC_IRQChannelSubPriority = 0;
   NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE;
   NVIC_Init(&NVIC_InitStructure);
@@ -45,7 +45,7 @@ void BSP_NVIC_InitConfig(void)
 	
 	//TIM2
 	NVIC_InitStructure.NVIC_IRQChannel=TIM2_IRQn;
-	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=1;//抢占优先级
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=9;//抢占优先级
 	NVIC_InitStructure.NVIC_IRQChannelSubPriority=0;//子优先级
 	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
 	NVIC_Init(&NVIC_InitStructure);
