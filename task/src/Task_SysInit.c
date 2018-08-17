1cc17709f6ac8d510c5b1a593af201d21c7041ec
tccoin <1774241372@qq.com>
2018-08-17 19:57:15 +0800

    0.1.15 禁用tim6

--- task/src/Task_SysInit.c
+++ task/src/Task_SysInit.c
@@ -17,18 +17,14 @@ void Task_SysInit(void *Parameters) {
     BSP_TIM_InitConfig();
     BSP_NVIC_InitConfig();
 
-    // 调试相关
-    delay_init(180); // 延时初始化
-    uart_init(9600); // 初始化串口
-    LED_Init();      // 初始化LED
-    BEEP_Init();     // 初始化蜂鸣器
-
     // 建立任务
     //// IRQ任务
-    xTaskCreate(Task_Debug, "Task_Debug", 500, NULL, 6, &TaskHandler_Debug);
-    xTaskCreate(Task_DBUS, "Task_DBUS", 400, NULL, 6, &TaskHandler_DBUS);
+    //xTaskCreate(Task_Debug, "Task_Debug", 500, NULL, 6, &TaskHandler_Debug);
+    //xTaskCreate(Task_DBUS, "Task_DBUS", 400, NULL, 6, &TaskHandler_DBUS);
     //// 低优先级任务
     xTaskCreate(Task_Blink, "Task_Blink", 400, NULL, 3, &TaskHandler_Blink);
+
+    printf("4");
 
     // 完成使命
     vTaskDelete(NULL);
