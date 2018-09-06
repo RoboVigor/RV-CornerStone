0e4b06d29019de097fbe34e8d6b6f259bf414de8
tccoin <1774241372@qq.com>
2018-09-06 21:19:40 +0800

    排查CAN错误

--- bsp/src/BSP_NVIC.c
+++ bsp/src/BSP_NVIC.c
@@ -20,10 +20,10 @@ void BSP_NVIC_Init(void) {
     // CAN1
     NVIC_InitStructure.NVIC_IRQChannel                   = CAN1_RX0_IRQn;
     NVIC_InitStructure.NVIC_IRQChannelCmd                = ENABLE;
-    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 8;
+    NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 10;
     NVIC_InitStructure.NVIC_IRQChannelSubPriority        = 0;
     NVIC_Init(&NVIC_InitStructure);
-    CAN_ITConfig(CAN1, CAN_IT_FMP0, ENABLE);
+    CAN_ITConfig(CAN1, CAN_IT_EWG | CAN_IT_EPV | CAN_IT_BOF | CAN_IT_LEC | CAN_IT_ERR | CAN_IT_WKU | CAN_IT_SLK, ENABLE);
 
     // CAN2
     NVIC_InitStructure.NVIC_IRQChannel                   = CAN2_RX0_IRQn;
