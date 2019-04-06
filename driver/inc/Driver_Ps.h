060014506655c2d53190f39f77acdac7df689d8e
tccoin <1774241372@qq.com>
2019-04-07 03:22:26 +0800

    update driver_ps

--- driver/inc/Driver_Ps.h
+++ driver/inc/Driver_Ps.h
@@ -8,28 +8,22 @@
 
 #include "stm32f4xx.h"
 
-#define SOP (uint8_t)87 // start of package
-#define EOP (uint8_t)88 // end of package
+#define SOP (uint8_t) 87 // start of package
+#define EOP (uint8_t) 88 // end of package
 
 typedef struct {
-  uint8_t data[34];
-  uint16_t result[10];
-  uint32_t id;
+    uint8_t  data[34];
+    uint16_t result[10];
+    uint8_t  id;  // command id
+    uint8_t  len; // command id
+    uint32_t pid; // package id
 } PsData_Type;
 
-void Ps_Append(PsData_Type *PsData, uint8_t value);
+void Ps_Update(PsData_Type *PsData, uint8_t newByte);
 void Ps_Parse_Header(PsData_Type *PsData, uint8_t value);
-void Ps_Valid(PsData_Type *PsData);
+void Ps_Parse_Data(PsData_Type *PsData, uint8_t *data);
+void Ps_Valid(PsData_Type *PsData, uint8_t crc);
 void Ps_Reset(PsData_Type *PsData);
-
-void Ps_On_Start(PsData_Type *PsData);
-void Ps_On_Done(PsData_Type *PsData);
-void Ps_On_Interrupted(PsData_Type *PsData);
-void Ps_On_Received(PsData_Type *PsData, uint8_t newByte);
-
-void Ps_DataAnalysis(PsData_Type *PsData);
-void Ps_DataUnused(PsData_Type *PsData);
-void Ps_DataUsed(PsData_Type *PsData);
 
 uint8_t CRC_Calculate(uint8_t *data, uint8_t len);
 uint8_t CRC_Valid(uint8_t *data, uint8_t len, uint8_t crc);
