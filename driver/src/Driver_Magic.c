1a2d3ffc239186250fb1fc8beedf3fbc359fd057
tccoin <1774241372@qq.com>
2019-04-21 02:01:07 +0800

    添加默认串口调试端口设置

--- driver/src/Driver_Magic.c
+++ driver/src/Driver_Magic.c
@@ -5,50 +5,53 @@
 
 //标准库需要的支持函数
 struct __FILE {
-  int handle;
+    int handle;
 };
 
 FILE __stdout;
 
 //重定义fputc函数
 int fputc(int ch, FILE *f) {
-  while ((USART6->SR & 0X40) == 0)
-    ; //循环发送,直到发送完毕
-  USART6->DR = (u8)ch;
-  return ch;
+#ifndef SERIAL_DEBUG_PORT
+#define SERIAL_DEBUG_PORT USART6
+#endif
+    //循环发送,直到发送完毕
+    while ((SERIAL_DEBUG_PORT->SR & 0X40) == 0) {
+    }
+    SERIAL_DEBUG_PORT->DR = (u8) ch;
+    return ch;
 }
 
 void Magic_Init_Handle(MagicHandle_Type *magic, int defaultValue) {
-  magic->defaultValue = defaultValue;
+    magic->defaultValue = defaultValue;
 }
 
-int lastResult = -1; // 记录接收到的调试数据
+int lastResult   = -1; // 记录接收到的调试数据
 int receivedSign = 0;
 
 void Magic_Get_Debug_Value(MagicHandle_Type *magic) {
-  int result = 0;
-  int length = 0;
+    int result = 0;
+    int length = 0;
 
-  if ((magic->sta & 0x8000) > 0) { // 串口接收到调试数据
-    uint8_t i = 0;
+    if ((magic->sta & 0x8000) > 0) { // 串口接收到调试数据
+        uint8_t i = 0;
 
-    length = magic->sta - 0xC000; // 调试数据的长度
-    result = 0;                   // 调试数据
+        length = magic->sta - 0xC000; // 调试数据的长度
+        result = 0;                   // 调试数据
 
-    for (i = 0; i < length; i++) {
-      result +=
-          pow(10.0, length - i - 1) * (magic->buf[i] - 48); // 将串口数据转码
-      magic->buf[i] = 0; // 清空数据缓存
+        for (i = 0; i < length; i++) {
+            result += pow(10.0, length - i - 1) * (magic->buf[i] - 48); // 将串口数据转码
+            magic->buf[i] = 0;                                          // 清空数据缓存
+        }
+        magic->sta   = 0;      // 清空串口的接收缓存
+        lastResult   = result; // 记录接收到的调试数据
+        receivedSign = 1;
+    } else {                              // 串口 没有 接收到调试数据
+        if (receivedSign == 1) {          // 之前 已经 接收过数据
+            result = lastResult;          // 返回上一次接收的调试数据
+        } else {                          // 之前 没有 接收过数据
+            result = magic->defaultValue; // 返回设定的默认值
+        }
     }
-    magic->sta = 0;      // 清空串口的接收缓存
-    lastResult = result; // 记录接收到的调试数据
-    receivedSign = 1;
-  } else {                          // 串口 没有 接收到调试数据
-    if (receivedSign == 1) {        // 之前 已经 接收过数据
-      result = lastResult;          // 返回上一次接收的调试数据
-    } else {                        // 之前 没有 接收过数据
-      result = magic->defaultValue; // 返回设定的默认值
-    }
-  }
-  magic->value = result;
+    magic->value = result;
 }
--- user/template/config.h
+++ user/template/config.h
@@ -6,13 +6,14 @@
 #define DRIVER_VERSION_1_0 // 适配版本1.0
 
 // 硬件
-#define DEBUG_ENABLED 1       // 调试开关
-#define BOARD_VERSION 1       // 开发板型号, 0:旧板, 1:A板
-#define USER_POWER_ENABLED 1  // 24V用户电源开关
-#define USART3_ENABLED 1      // 串口3开关
-#define USART3_BAUD_RATE 9600 // 串口3波特率
-#define USART6_ENABLED 1      // 串口6开关
-#define USART6_BAUD_RATE 9600 // 串口6波特率
+#define DEBUG_ENABLED 1          // 调试开关
+#define BOARD_VERSION 1          // 开发板型号, 0:旧板, 1:A板
+#define USER_POWER_ENABLED 1     // 24V用户电源开关
+#define USART3_ENABLED 1         // 串口3开关
+#define USART3_BAUD_RATE 9600    // 串口3波特率
+#define USART6_ENABLED 1         // 串口6开关
+#define USART6_BAUD_RATE 9600    // 串口6波特率
+#define SERIAL_DEBUG_PORT USART6 // 串口调试端口
 
 // 陀螺仪
 #define GYROSCOPE_YAW_START_UP_DELAY_ENABLED 1
