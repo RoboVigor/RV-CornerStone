2d0297e20f9e06aa7fa062b276103c5078c4ab40
ChetYoung <719506526@qq.com>
2019-05-26 00:07:19 +0800

    按板子的摆放赋值

--- driver/src/Driver_Gyroscope.c
+++ driver/src/Driver_Gyroscope.c
@@ -35,18 +35,31 @@ void Gyroscope_Init(GyroscopeData_Type *GyroscopeData) {
 // MPU6500数据读取,成功返回1  失败返回0
 int Gyroscope_Update(GyroscopeData_Type *GyroscopeData) {
     static uint8_t mpu_buf[20];
+    uint8_t        changeData;
 
     //尝试读取数据
     if (IIC_ReadData(MPU_IIC_ADDR, MPU6500_ACCEL_XOUT_H, mpu_buf, 14) == 0xff) return 0;
 
     //成功的话进行赋值
-    ImuData.ax   = (((int16_t) mpu_buf[0]) << 8) | mpu_buf[1];
-    ImuData.ay   = (((int16_t) mpu_buf[2]) << 8) | mpu_buf[3];
-    ImuData.az   = (((int16_t) mpu_buf[4]) << 8) | mpu_buf[5];
     ImuData.temp = (((int16_t) mpu_buf[6]) << 8) | mpu_buf[7];
-    ImuData.gx   = ((((int16_t) mpu_buf[8]) << 8) | mpu_buf[9]) - IMU_GX_BIAS;
-    ImuData.gy   = ((((int16_t) mpu_buf[10]) << 8) | mpu_buf[11]) - IMU_GY_BIAS;
-    ImuData.gz   = ((((int16_t) mpu_buf[12]) << 8) | mpu_buf[13]) - IMU_GZ_BIAS;
+#if BOARD_FRONT_IS_UP
+    ImuData.az = (((int16_t) mpu_buf[4]) << 8) | mpu_buf[5];
+    ImuData.gz = ((((int16_t) mpu_buf[12]) << 8) | mpu_buf[13]) - IMU_GZ_BIAS;
+#else
+    ImuData.az = -1 * (((int16_t) mpu_buf[4]) << 8) | mpu_buf[5];
+    ImuData.gz = -1 * ((((int16_t) mpu_buf[12]) << 8) | mpu_buf[13]) - IMU_GZ_BIAS;
+#endif
+#if BOARD_SHORT_SIDE_IS_PARALLEL_TO_PITCH
+    ImuData.ax = (((int16_t) mpu_buf[0]) << 8) | mpu_buf[1];
+    ImuData.ay = (((int16_t) mpu_buf[2]) << 8) | mpu_buf[3];
+    ImuData.gx = ((((int16_t) mpu_buf[8]) << 8) | mpu_buf[9]) - IMU_GX_BIAS;
+    ImuData.gy = ((((int16_t) mpu_buf[10]) << 8) | mpu_buf[11]) - IMU_GY_BIAS;
+#else
+    ImuData.ay = (((int16_t) mpu_buf[0]) << 8) | mpu_buf[1];
+    ImuData.ax = (((int16_t) mpu_buf[2]) << 8) | mpu_buf[3];
+    ImuData.gy = ((((int16_t) mpu_buf[8]) << 8) | mpu_buf[9]) - IMU_GX_BIAS;
+    ImuData.gx = ((((int16_t) mpu_buf[10]) << 8) | mpu_buf[11]) - IMU_GY_BIAS;
+#endif
 
     // 读取完成进行解算
     Gyroscope_Solve(GyroscopeData);
