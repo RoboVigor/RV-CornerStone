a0148d9f9edec2bf9db9ad56e934aeeb70f5d076
ChetYoung <44935696+ChetYoung@users.noreply.github.com>
2020-06-16 21:09:00 +0800

    把snail初始化注释了

--- user/loop-infantry/bsp.c
+++ user/loop-infantry/bsp.c
@@ -34,8 +34,8 @@ void BSP_Init(void) {
     BSP_PWM_Init(&PWM_Magazine_Servo, 9000, 200, TIM_OCPolarity_Low);
 
     // snail 电机pwm输出
-    BSP_PWM_Set_Port(&PWM_Snail1, PWM_PORT_PD12);
-    BSP_PWM_Init(&PWM_Snail1, 180, 1250, TIM_OCPolarity_Low);
-    BSP_PWM_Set_Port(&PWM_Snail2, PWM_PORT_PD13);
-    BSP_PWM_Init(&PWM_Snail2, 180, 1250, TIM_OCPolarity_Low);
+    // BSP_PWM_Set_Port(&PWM_Snail1, PWM_PORT_PD12);
+    // BSP_PWM_Init(&PWM_Snail1, 180, 1250, TIM_OCPolarity_Low);
+    // BSP_PWM_Set_Port(&PWM_Snail2, PWM_PORT_PD13);
+    // BSP_PWM_Init(&PWM_Snail2, 180, 1250, TIM_OCPolarity_Low);
 }
