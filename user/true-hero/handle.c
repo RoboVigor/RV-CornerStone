bdb47b1f13acc0160aa493c4d9f79f782a2eebe3
PiggyLiu <47034031+PiggyLiu@users.noreply.github.com>
2020-07-12 13:31:38 +0800

    适配适配适配

--- user/true-hero/handle.c
+++ user/true-hero/handle.c
@@ -4,24 +4,38 @@
  */
 #include "handle.h"
 #include "config.h"
+#include "macro.h"
 
 void Handle_Init(void) {
     // 底盘电机
-    Motor_Init(&Motor_LF, CHASSIS_MOTOR_REDUCTION_RATE, 0);
-    Motor_Init(&Motor_LB, CHASSIS_MOTOR_REDUCTION_RATE, 0);
-    Motor_Init(&Motor_RB, CHASSIS_MOTOR_REDUCTION_RATE, 0);
-    Motor_Init(&Motor_RF, CHASSIS_MOTOR_REDUCTION_RATE, 0);
+    Motor_Init(&Motor_LF, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, ENABLE);
+    Motor_Init(&Motor_LB, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, ENABLE);
+    Motor_Init(&Motor_RB, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, ENABLE);
+    Motor_Init(&Motor_RF, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, ENABLE);
 
     // 发射机构电机
-    Motor_Init(&Motor_LeftFrict, 1, 0);
-    Motor_Init(&Motor_RightFrict, 1, 0);
-    Motor_Init(&Motor_Stir3510, 19.2, 0); // 19.2
+    Motor_Init(&Motor_LeftFrict, 1, DISABLE, ENABLE);
+    Motor_Init(&Motor_LeftFrict, 1, DISABLE, ENABLE);
+    Motor_Init(&Motor_LeftFrict, 19.2, DISABLE, ENABLE);
 
     // 云台电机
-    Motor_Init(&Motor_Yaw, 1.0, 1);   // 顺时针为正电流
-    Motor_Init(&Motor_Pitch, 1.0, 1); // 逆时针为正电流
-    // Motor_Yaw.positionBias = 4041;    // 167
-    // Motor_Yaw.position     = 4041;    // 167
+    Motor_Init(&Motor_Yaw, 1, ENABLE, ENABLE); // 顺时针为正电流
+    Motor_Init(&Motor_Yaw, 1, ENABLE, ENABLE); // 逆时针为正电流
+
+    // CAN外设
+    Can1_Device[ESC_ID(0x203)] = &Motor_Yaw;
+    Can1_Device[ESC_ID(0x204)] = &Motor_Stir3510;
+    Can1_Device[ESC_ID(0x205)] = &Motor_LF;
+    Can1_Device[ESC_ID(0x206)] = &Motor_LB;
+    Can1_Device[ESC_ID(0x207)] = &Motor_RB;
+    Can1_Device[ESC_ID(0x208)] = &Motor_RF;
+
+    Can2_Device[ESC_ID(0x201)] = &Motor_LeftFrict;
+    Can2_Device[ESC_ID(0x202)] = &Motor_RightFrict;
+    Can2_Device[ESC_ID(0x206)] = &Motor_Pitch;
+
+    // 陀螺仪设置静态误差
+    Gyroscope_Set_Bias(&ImuData, 0.15, 34.65, -8.55);
 
     // 遥控器数据初始化
     DBUS_Init(&remoteData, &keyboardData, &mouseData);
@@ -30,6 +44,7 @@ void Handle_Init(void) {
     Magic_Init(&magic, 0);
 
     // 通讯协议初始化
-    Protocol_Init(&Judge);
-    Protocol_Init(&Ps);
+    Protocol_Init(&JudgeChannel, &ProtocolData);
+    Protocol_Init(&HostChannel, &ProtocolData);
+    Protocol_Init(&UserChannel, &ProtocolData);
 }
