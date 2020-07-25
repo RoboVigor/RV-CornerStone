19df87ede5cb0cb9c27be8428fbeaeff364fc43a
ChetYoung <44935696+ChetYoung@users.noreply.github.com>
2020-07-25 23:41:35 +0800

    结算采样频率更新

--- user/dart/config.h
+++ user/dart/config.h
@@ -16,13 +16,15 @@
 #define GYROSCOPE_LSB 16.384f  // 陀螺仪敏感度
 #define ACCELERATE_LSB 4096.0f // 加速度计敏感度
 #define GYROSCOPE_FREQ 0x03    // 陀螺仪频率 0x09/100Hz 0x04/200Hz 0x03/250Hz 0x01/500Hz 0x00/1000Hz
+#define SAMPLE_FREQ 250.0f     // 结算采样频率
 #endif
 #ifdef STM32F407xx
 #define ACCELERATE_LSB 1114.3f // 加速度计敏感度
 #define GYROSCOPE_LSB 938.7f   // 陀螺仪敏感度
 #define MAGNETIC_LSB 3.33f     // 磁力计敏感度
-#define ACCELERATE_FREQ 0x09   // 加速度计频率 0x08/100Hz 0x09/200Hz 0xA/400Hz 0xB/800Hz
+#define ACCELERATE_FREQ 0x09   // 加速度计频率 0x08/100Hz 0x09/200Hz 0x0A/400Hz 0x0B/800Hz
 #define GYROSCOPE_FREQ 0x04    // 陀螺仪频率 0x05/100Hz 0x04/200Hz 0x03/400Hz 0x02/1000Hz
+#define SAMPLE_FREQ 200.0f     // 结算采样频率
 #endif
 
 // DMA
