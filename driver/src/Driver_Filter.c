d6f662dc71477e104afe876ff38ec907742f111e
CP-Nelson <CPNelson@163.com>
2020-10-04 23:12:30 +0800

    适配微调，还不能成功平衡

--- driver/src/Driver_Filter.c
+++ driver/src/Driver_Filter.c
@@ -2,6 +2,7 @@
 #include "macro.h"
 
 void Filter_Update(Filter_Type *filter, float value) {
+    int i;
     // 初始化
     if (filter->count == 0) {
         filter->count     = 1;
@@ -10,6 +11,12 @@ void Filter_Update(Filter_Type *filter, float value) {
         filter->value     = value;
         filter->lastValue = value;
         filter->result    = value;
+        if (filter->windowSize > 0) {
+            filter->movingAverageArray = malloc(4 * filter->windowSize);
+            for (i = 0; i < filter->windowSize; i++) {
+                *(filter->movingAverageArray + i) = value;
+            }
+        }
         return;
     }
 
@@ -18,12 +25,22 @@ void Filter_Update(Filter_Type *filter, float value) {
     filter->value     = value;
     filter->diff      = filter->value - filter->lastValue;
     filter->count += 1;
+    *(filter->movingAverageArray + filter->count % filter->windowSize) = value;
 }
 
 void Filter_Update_Sample(Filter_Type *filter) {
-    filter->movingAverage = (float) (filter->movingAverage * (filter->count - 1) + filter->value) / (float) filter->count;
-    filter->max           = MAX(filter->max, filter->value);
-    filter->min           = MIN(filter->min, filter->value);
+    filter->average = (float) (filter->movingAverage * (filter->count - 1) + filter->value) / (float) filter->count;
+    filter->max     = MAX(filter->max, filter->value);
+    filter->min     = MIN(filter->min, filter->value);
+}
+
+void Filter_Update_Moving_Average(Filter_Type *filter) {
+    int   i;
+    float sum;
+    for (i = 0; i < filter->windowSize; i++) {
+        sum += *(filter->movingAverageArray + i);
+    }
+    filter->movingAverage = sum / (float) filter->windowSize;
 }
 
 float Filter_Apply_Limit_Breadth(Filter_Type *filter) {
