f6e665ddfb6cad83bc3fcefc7e8eafd842b1c123
CP-Nelson <CPNelson@163.com>
2021-01-14 15:18:17 +0800

    下云台电机驱动尝试
    1,更改了下云台的pitch轴，yaw轴电机的减速比
    2, yaw轴平滑转动，pitch轴有上下极值数据问题
    pitch轴电机在最大范围的上下限位置给的值太大了，超过了数据限制，尝试增加数值限位失败
    
    Co-Authored-By: 2027308688 <58624029+2027308688@users.noreply.github.com>
    Co-Authored-By: LW4001 <76775293+LW4001@users.noreply.github.com>

--- user/sentry/handle.c
+++ user/sentry/handle.c
@@ -8,30 +8,30 @@
 
 void Handle_Init(void) {
     // 底盘电机
-    Motor_Init(&Motor_Chassis_Left, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, ENABLE);
-    Motor_Init(&Motor_Chassis_Right, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, ENABLE);
+    Motor_Init(&Motor_Chassis_Left, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, DISABLE);
+    Motor_Init(&Motor_Chassis_Right, CHASSIS_MOTOR_REDUCTION_RATE, DISABLE, DISABLE);
 
     // 云台电机
     // 上
-    Motor_Init(&Motor_Up_Gimbal_Yaw, 36, ENABLE, ENABLE);
-    Motor_Init(&Motor_Up_Gimbal_Pitch, 36, ENABLE, ENABLE);
+    Motor_Init(&Motor_Up_Gimbal_Yaw, 36, DISABLE, DISABLE);
+    Motor_Init(&Motor_Up_Gimbal_Pitch, 36, DISABLE, DISABLE);
     // 下
-    Motor_Init(&Motor_Down_Gimbal_Yaw, 36, ENABLE, ENABLE);
-    Motor_Init(&Motor_Down_Gimbal_Pitch, 36, ENABLE, ENABLE);
+    Motor_Init(&Motor_Down_Gimbal_Yaw, 1, ENABLE, ENABLE);
+    Motor_Init(&Motor_Down_Gimbal_Pitch, 1, ENABLE, ENABLE);
 
     // 摩擦轮电机
     // 上
-    Motor_Init(&Motor_Up_Frict_Left, 1, DISABLE, ENABLE);
-    Motor_Init(&Motor_Up_Frict_Right, 1, DISABLE, ENABLE);
+    Motor_Init(&Motor_Up_Frict_Left, 1, DISABLE, DISABLE);
+    Motor_Init(&Motor_Up_Frict_Right, 1, DISABLE, DISABLE);
     // 下
-    Motor_Init(&Motor_Down_Frict_Left, 1, DISABLE, ENABLE);
-    Motor_Init(&Motor_Down_Frict_Right, 1, DISABLE, ENABLE);
+    Motor_Init(&Motor_Down_Frict_Left, 1, DISABLE, DISABLE);
+    Motor_Init(&Motor_Down_Frict_Right, 1, DISABLE, DISABLE);
 
     // 拨弹电机
     // 上
-    Motor_Init(&Motor_Up_Stir, 1, DISABLE, ENABLE);
+    Motor_Init(&Motor_Up_Stir, 1, DISABLE, DISABLE);
     // 下
-    Motor_Init(&Motor_Down_Stir, 1, DISABLE, ENABLE);
+    Motor_Init(&Motor_Down_Stir, 1, DISABLE, DISABLE);
 
     // Motor_Up_Gimbal_Pitch.positionBias   = ;
     // Motor_Up_Gimbal_Pitch.position       = ;
@@ -52,8 +52,8 @@ void Handle_Init(void) {
     Can1_Device[ESC_ID(0x203)] = &Motor_Down_Frict_Left;
     Can1_Device[ESC_ID(0x204)] = &Motor_Down_Frict_Right;
     Can1_Device[ESC_ID(0x205)] = &Motor_Down_Stir;
-    Can1_Device[ESC_ID(0x206)] = &Motor_Down_Gimbal_Yaw;
-    Can1_Device[ESC_ID(0x207)] = &Motor_Down_Gimbal_Pitch;
+    Can2_Device[ESC_ID(0x207)] = &Motor_Down_Gimbal_Yaw;
+    Can1_Device[ESC_ID(0x209)] = &Motor_Down_Gimbal_Pitch;
 
     Can2_Device[ESC_ID(0x203)] = &Motor_Up_Frict_Left;
     Can2_Device[ESC_ID(0x204)] = &Motor_Up_Frict_Right;
