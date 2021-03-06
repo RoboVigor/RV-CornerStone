7baf0e19e6346a7f7aad1635d5877bc0f7895893
Rocky_ <1315930212@qq.com>
2021-03-07 01:01:49 +0800

    launcher完成CornerStone 2.0实配, MDK可通过编译

--- user/launcher/tasks.h
+++ user/launcher/tasks.h
@@ -1,17 +1,13 @@
 #ifndef __TASK_SYSTEMINIT_H
 #define __TASK_SYSTEMINIT_H
 
-#if DEBUG_ENABLED
-void Task_Debug_Magic_Send(void *Parameters);         // 无线串口调试输出
-void Task_Debug_Magic_Receive(void *Parameters);      // 无线串口调试接收
-void Task_Debug_RTOS_State(void *Parameters);         // 输出RTOS系统资源
-void Task_Debug_Gyroscope_Sampling(void *Parameters); // 采样任务
-#endif
-
-void Task_Sys_Init(void *Parameters);  // 硬件初始化和任务初始化
-void Task_Blink(void *Parameters);     // 确认存活
-void Task_Chassis(void *Parameters);   // 底盘运动
-void Task_Safe_Mode(void *Parameters); // 安全模式
-void Task_Gyroscope(void *Parameters); // 陀螺仪姿态解算
+void Task_Blink(void *Parameters);         // 确认存活
+void Task_Control(void *Parameters);       // 手柄和键盘的控制
+void Task_Communication(void *Parameters); // 协议通讯
+void Task_Hook(void *Parameters);          // 载飞镖架的固定与释放
+void Task_Charge(void *Parameters);        // 飞镖架的蓄能
+void Task_Safe_Mode(void *Parameters);     // 安全模式
+void Task_Startup_Music(void *Parameters); // 开机音乐
+void Task_Can_Send(void *Parameters);      // CAN发送任务
 
 #endif
