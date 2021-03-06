259730f67f90ced51eecda53b023297634f4553c
Rocky_ <1315930212@qq.com>
2021-03-07 01:01:49 +0800

    launcher完成CornerStone 2.0实配, MDK可通过编译

--- user/launcher/main.c
+++ user/launcher/main.c
@@ -1,5 +1,24 @@
 #define __HANDLE_GLOBALS
-#include "main.h"
+
+#include "config.h"
+#include "macro.h"
+#include "handle.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "tasks.h"
+
+void BSP_Switch_Init(void) {
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOI, ENABLE);
+
+    GPIO_InitStructure.GPIO_Mode  = GPIO_Mode_IN;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_Pin   = GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_UP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
+    GPIO_Init(GPIOI, &GPIO_InitStructure);
+}
 
 int main(void) {
 
@@ -11,10 +30,73 @@ int main(void) {
     LED_Init();      // 初始化LED
     Beep_Init();     // 初始化蜂鸣器
 
-    //创建系统初始化任务
-    xTaskCreate(Task_Sys_Init, "Task_Sys_Init", 400, NULL, 1, NULL);
+    /*******************************************************************************
+     *                                  硬件初始化                                  *
+     *******************************************************************************/
 
-    //启动调度,开始执行任务
+    // 获得设备ID
+    BSP_Stone_Id_Init(&Board_Id, &Robot_Id);
+
+    // 卷绳电机
+    Motor_Init(&Motor_Charge, 1, DISABLE, ENABLE);
+
+    // 遥控器数据初始化
+    DBUS_Init(&remoteData, &keyboardData, &mouseData);
+
+    // 通讯协议初始化
+    Protocol_Init(&Node_Judge, &ProtocolData);
+    Protocol_Init(&Node_Host, &ProtocolData);
+    Protocol_Init(&Node_Board, &ProtocolData);
+
+    // 硬件配置
+    BSP_CAN_Init();
+    BSP_DBUS_Init(remoteBuffer);
+    BSP_TIM2_Init();
+    BSP_IMU_Init();
+    BSP_Laser_Init();
+    BSP_Beep_Init();
+    BSP_LED_Init();
+    BSP_User_Power_Init();
+    BSP_Switch_Init();
+
+    // USART
+    BSP_USART6_Init(115200, USART_IT_IDLE);
+    BSP_UART7_Init(115200, USART_IT_IDLE);
+    // BSP_UART8_Init(115200, USART_IT_IDLE);
+
+    // PWM
+    BSP_PWM_Set_Port(&PWM_Hook_L, PWM_PA1);
+    BSP_PWM_Init(&PWM_Hook_L, 9000, 200, TIM_OCPolarity_Low);
+    BSP_PWM_Set_Port(&PWM_Hook_R, PWM_PA2);
+    BSP_PWM_Init(&PWM_Hook_R, 9000, 200, TIM_OCPolarity_Low);
+
+    // ADC
+
+    // 总线设置
+    Bridge_Bind(&BridgeData, CAN1_BRIDGE, 0x201, &Motor_Charge);
+    Bridge_Bind(&BridgeData, USART_BRIDGE, 6, &Node_Judge);
+    Bridge_Bind(&BridgeData, USART_BRIDGE, 7, &Node_Board);
+    // Bridge_Bind(&BridgeData, USART_BRIDGE, 8, &Node_Host);
+
+    /*******************************************************************************
+     *                                 任务初始化                                   *
+     *******************************************************************************/
+
+    // 低优先级任务
+    xTaskCreate(Task_Blink, "Task_Blink", 400, NULL, 3, NULL);
+    // xTaskCreate(Task_Startup_Music, "Task_Startup_Music", 400, NULL, 3, NULL);
+
+    // 等待遥控器开启
+    // while (!remoteData.state) {
+    // }
+
+    // 高优先级任务
+    xTaskCreate(Task_Charge, "Task_Charge", 400, NULL, 6, NULL);               // 发射架蓄能任务
+    xTaskCreate(Task_Hook, "Task_Hook", 400, NULL, 6, NULL);                   // 载飞镖模块固定与释放任务
+    xTaskCreate(Task_Control, "Task_Control", 400, NULL, 9, NULL);             // 遥控器控制任务
+    xTaskCreate(Task_Communication, "Task_Communication", 500, NULL, 6, NULL); // 通讯任务
+    xTaskCreate(Task_Can_Send, "Task_Can_Send", 500, NULL, 6, NULL);           // Can发送任务
+
     vTaskStartScheduler();
 
     //系统启动失败:定时器任务或者空闲任务的heap空间不足
--- user/launcher/tasks.c
+++ user/launcher/tasks.c
@@ -2,7 +2,11 @@
  * @brief 甩锅小车
  * @version 1.2.0
  */
-#include "main.h"
+
+#include "config.h"
+#include "macro.h"
+#include "handle.h"
+#include "tasks.h"
 
 void Task_Control(void *Parameters) {
     TickType_t LastWakeTime = xTaskGetTickCount();
@@ -15,49 +19,29 @@ void Task_Control(void *Parameters) {
     vTaskDelete(NULL);
 }
 
-void Task_Board_Communication(void *Parameters) {
-    TickType_t LastWakeTime = xTaskGetTickCount(); // 时钟
-    float      interval     = 0.1;                 // 任务运行间隔 s
-    int        intervalms   = interval * 1000;     // 任务运行间隔 ms
+void Task_Communication(void *Parameters) {
+    TickType_t         LastWakeTime = xTaskGetTickCount(); // 时钟
+    float              interval     = 0.1;                 // 任务运行间隔 s
+    int                intervalms   = interval * 1000;     // 任务运行间隔 ms
+    ProtocolInfo_Type *protocolInfo = Protocol_Get_Info_Handle(0x501);
 
     while (1) {
-        uint16_t id;
-        uint16_t dataLength;
-        uint16_t offset = 0;
 
-        // 板间通信
-#ifdef BOARD_ALPHA
-        id                                 = 0x501;
-        ProtocolData.user.boardAlpha.data1 = 1.11;
-        ProtocolData.user.boardAlpha.data2 = 2.22;
-        ProtocolData.user.boardAlpha.data3 = 3.33;
-        ProtocolData.user.boardAlpha.data4 = 4.44;
-#endif
-
-#ifdef BOARD_BETA
-        id                                = 0x502;
-        ProtocolData.user.boardBeta.data1 = 0;
-        ProtocolData.user.boardBeta.data2 = 0;
-        ProtocolData.user.boardBeta.data3 = 0;
-        ProtocolData.user.boardBeta.data4 = 1.11;
-#endif
-
-        // USART发送
-        DMA_Disable(UART7_Tx);
-        Protocol_Get_Packet_Info(id, &offset, &dataLength);
-        dataLength = Protocol_Pack(&UserChannel, id);
-        DMA_Enable(UART7_Tx, PROTOCOL_HEADER_CRC_CMDID_LEN + dataLength);
-
-        // Can发送
-        dataLength = Protocol_Pack(&UserChannel, id);
-        Can_Send_Msg(CAN1, id, UserChannel.sendBuf, PROTOCOL_HEADER_CRC_CMDID_LEN + dataLength);
+        // 修改数据
+        ProtocolData.boardAlpha.data1 = 1;
+        ProtocolData.boardAlpha.data2 = 2;
+        ProtocolData.boardAlpha.data3 = 3;
+        ProtocolData.boardAlpha.data4 = 4;
+        Bridge_Send_Protocol_Once(&Node_Host, 0x501);
 
         // 发送频率
         vTaskDelayUntil(&LastWakeTime, intervalms);
 
         // 调试信息
-        // DebugData.debug1 = ProtocolData.user.boardAlpha.data1 * 1000;
-        // DebugData.debug2 = ProtocolData.user.boardBeta.data4 * 1000;
+        DebugData.debug1 = ProtocolData.boardAlpha.data1 * 1000;
+        DebugData.debug2 = ProtocolData.boardAlpha.data2 * 1000;
+        DebugData.debug3 = Node_Host.sendSeq;
+        DebugData.debug4 = protocolInfo->receiveCount;
     }
     vTaskDelete(NULL);
 }
@@ -113,35 +97,9 @@ void Task_Can_Send(void *Parameters) {
     float      interval     = 0.01;                // 任务运行间隔 s
     int        intervalms   = interval * 1000;     // 任务运行间隔 ms
 
-    CAN_TypeDef *Canx[2]          = {CAN1, CAN2};
-    Motor_Type **Canx_Device[2]   = {Can1_Device, Can2_Device};
-    uint16_t     Can_Send_Id[3]   = {0x200, 0x1ff, 0x2ff};
-    uint16_t     Can_ESC_Id[3][4] = {{0x201, 0x202, 0x203, 0x204}, {0x205, 0x206, 0x207, 0x208}, {0x209, 0x020a, 0x20b, 0x20c}};
-
-    int         i, j, k;        // CAN序号 发送ID序号 电调ID序号
-    int         isNotEmpty = 0; // 同一发送ID下是否有电机
-    Motor_Type *motor;          // 根据i,j,k锁定电机
-    int16_t     currents[4];    // CAN发送电流
-
     while (1) {
-        for (i = 0; i < 2; i++) {
-            for (j = 0; j < 3; j++) {
-                isNotEmpty = 0;
-                for (k = 0; k < 4; k++) {
-                    motor       = *(Canx_Device[i] + ESC_ID(Can_ESC_Id[j][k]));
-                    currents[k] = (motor && motor->inputEnabled) ? motor->input : 0;
-                    isNotEmpty  = isNotEmpty || (motor && motor->inputEnabled);
-                }
-
-                if (isNotEmpty && !SafetyMode) {
-                    Can_Send(Canx[i], Can_Send_Id[j], currents[0], currents[1], currents[2], currents[3]);
-                } else if (isNotEmpty && SafetyMode) {
-                    Can_Send(Canx[i], Can_Send_Id[j], 0, 0, 0, 0);
-                }
-            }
-        }
-        // 发送频率
-        vTaskDelayUntil(&LastWakeTime, intervalms);
+        Bridge_Send_Motor(&BridgeData, SafetyMode);
+        vTaskDelayUntil(&LastWakeTime, intervalms); // 发送频率
     }
     vTaskDelete(NULL);
 }
@@ -163,47 +121,5 @@ void Task_Startup_Music(void *Parameters) {
         if (KTV_Play(Music_Soul)) break;
         vTaskDelayUntil(&LastWakeTime, 60);
     }
-    vTaskDelete(NULL);
-}
-
-void Task_Sys_Init(void *Parameters) {
-
-    // 初始化全局变量
-    Handle_Init();
-
-    // 初始化硬件
-    BSP_Init();
-
-    // 调试任务
-#if DEBUG_ENABLED
-    // xTaskCreate(Task_Debug_Magic_Receive, "Task_Debug_Magic_Receive", 500, NULL, 6, NULL);
-    // xTaskCreate(Task_Debug_Magic_Send, "Task_Debug_Magic_Send", 500, NULL, 6, NULL);
-    // xTaskCreate(Task_Debug_RTOS_State, "Task_Debug_RTOS_State", 500, NULL, 6, NULL);
-    // xTaskCreate(Task_Debug_Gyroscope_Sampling, "Task_Debug_Gyroscope_Sampling", 400, NULL, 6, NULL);
-#endif
-
-    // 低级任务
-    xTaskCreate(Task_Blink, "Task_Blink", 400, NULL, 3, NULL);
-    // xTaskCreate(Task_Startup_Music, "Task_Startup_Music", 400, NULL, 3, NULL);
-
-    // 等待遥控器开启
-    while (!remoteData.state) {
-    }
-
-    // 运动控制任务
-    xTaskCreate(Task_Charge, "Task_Charge", 400, NULL, 6, NULL);
-    xTaskCreate(Task_Hook, "Task_Hook", 400, NULL, 6, NULL);
-
-    // 遥控器控制任务
-    xTaskCreate(Task_Control, "Task_Control", 400, NULL, 9, NULL);
-
-    // DMA发送任务
-    // xTaskCreate(Task_Client_Communication, "Task_Client_Communication", 500, NULL, 6, NULL);
-    // xTaskCreate(Task_Board_Communication, "Task_Board_Communication", 500, NULL, 6, NULL);
-
-    // Can发送任务
-    xTaskCreate(Task_Can_Send, "Task_Can_Send", 500, NULL, 6, NULL);
-
-    // 完成使命
     vTaskDelete(NULL);
 }
